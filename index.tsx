5d6df6b749769e90507b196c43f7422160826fd7
yangning <ning@edison.tech>
2021-06-11 17:39:31 +0800

    add androidLayerType={'software'}

--- index.tsx
+++ index.tsx
@@ -180,6 +180,7 @@ export default class RNWebView extends Component<
         allowFileAccess
         allowingReadAccessToURL={"file://"}
         onMessage={this.onMessage}
+        androidLayerType={"software"}
       />
     );
   }
